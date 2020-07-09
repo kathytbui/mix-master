c241544375e21418d2313761e27c8f12fbf8fbc7
Kathy Bui <kathy.bui1992@gmail.com>
2020-07-08 18:08:35 -0600

    Add create spec

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,22 @@
+class SongsController < ApplicationController
+
+  def new
+    @artist = Artist.find(params[:id])
+  end
+
+  def create
+    artist = Artist.find(params[:id])
+    new_song = artist.songs.create(song_params)
+    redirect_to("/songs/#{new_song.id}")
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    @artist = Artist.find(@song.artist_id)
+  end
+
+  private
+  def song_params
+    params.permit(:title)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   post '/artists', to: 'artists#create'
   get '/artists/:id', to: 'artists#show'
   get 'artists/:id/edit', to: 'artists#edit'
-  post '/artists/:id', to: 'artists#update'
+  patch '/artists/:id', to: 'artists#update'
   delete '/artists/:id/edit', to: 'artists#destroy'
+
+  get 'songs', to: 'songs#index'
+  get '/artists/:id/songs/new', to: 'songs#new'
+  post '/artists/:id/songs', to: 'songs#create'
+  get '/songs/:id', to: 'songs#show'
+
+
 end
